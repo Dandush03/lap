83fda74b1586abba9d031975a3b6e0305cdcfe24
Daniel Laloush <daniel.laloush@influitive.com>
2021-06-27 12:56:38 -0400

    test

--- administration/entrypoint.sh
+++ administration/entrypoint.sh
@@ -4,10 +4,9 @@ set -e
 
 cp -r /usr/src/cache/node_modules/. /usr/src/app/node_modules/
 
-if [[ $RAILS_ENV == 'production' ]]; then
+if [[ $RAILS_ENV == 'productiosn' ]]; then
   yarn build
   serve -l 3001 -s build
 else
   exec "$@"
 fi
-  
